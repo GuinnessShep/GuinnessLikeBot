289f8c3c6b312e77caec448b46d818e7f806fb21
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-07-25 04:38:51 -0700

    Update GenerateDevices.py

--- bot/GenerateDevices.py
+++ bot/GenerateDevices.py
@@ -1,20 +1,22 @@
 import random
 import uuid
 
-# Ask for the number of lines to generate
-num_lines = int(input("How many lines would you like to generate? "))
+# Ask the user how many lines they want to generate
+num_lines = int(input("How many lines do you want to generate? "))
 
-# Open the file to write
-with open('devices.txt', 'w') as f:
-    # Generate the specified number of lines
+with open("devices.txt", "w") as f:
     for _ in range(num_lines):
-        # Generate the fields
-        field1 = '714' + str(random.randint(1000000000000000000, 9999999999999999999))
-        field2 = '714' + str(random.randint(1000000000000000000, 9999999999999999999))
-        field3 = str(uuid.uuid4())
-        field4 = ''.join([random.choice('0123456789abcdef') for _ in range(16)])
+        # Generate two random 19 digit numbers starting with 714
+        num1 = 7140000000000000000 + random.randint(0, 999999999999999999)
+        num2 = 7140000000000000000 + random.randint(0, 999999999999999999)
+        
+        # Generate a random UUID
+        id = uuid.uuid4()
 
-        # Write the line to the file
-        f.write(f'{field1}:{field2}:{field3}:{field4}\n')
+        # Generate a random 16 digit hexadecimal
+        hex = "%016x" % random.randint(0, 2**64-1)
+        
+        # Write the generated values to the file
+        f.write(f"{num1}:{num2}:{id}:{hex}\n")
 
-print(f"Generated {num_lines} lines in devices.txt.")
+print("File 'devices.txt' has been successfully generated with random data.")
