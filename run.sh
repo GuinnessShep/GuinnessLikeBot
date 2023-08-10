7f84dadc65087f4c3d55fa1537a0d04ff126a3a0
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-08-09 22:24:10 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check if Git is installed
+if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y git
+fi
+
+# Clone repository
+#git clone https://github.com/GuinnessShep/GuinnessLikeBot
+#cd GuinnessLikeBot
+
+# Check if Python 3 is installed
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y python3
+fi
+
+# Install required Python packages
+python3 -m pip install console httpx pyyaml pystyle
+python3 -m pip install -r requirements.txt
+
+# Run Python scripts
+python3 bot/GenerateDevices.py 50000
+
+# Ask if proxies should be used
+read -p "Would you like to use proxies? (yes/no) " USE_PROXIES
+
+if [[ "${USE_PROXIES}" = 'yes' ]]; then
+    python3 proxy/scraper.py
+    python3 proxy/pl.py
+fi
+
+python3 bot/Modern.py
